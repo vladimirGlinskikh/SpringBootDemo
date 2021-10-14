60f2a9f7bd56bb2b67e9d6d0a61628037070dcf7
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-10-14 22:21:20 +0600

    Saving students

---
+++ src/main/java/kz/zhelezyaka/springbootdemo/student/StudentConfig.java
@@ -0,0 +1,33 @@
+package kz.zhelezyaka.springbootdemo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student vladimir = new Student(
+                    1L,
+                    "Vladimir",
+                    "vladimir@gmail.com",
+                    LocalDate.of(1998, 01, 9),
+                    39);
+            Student olya = new Student(
+                    2L,
+                    "Olya",
+                    "olya@gmail.com",
+                    LocalDate.of(1990, 9, 10),
+                    38);
+
+            repository.saveAll(
+                    List.of(vladimir, olya));
+        };
+    }
+}
