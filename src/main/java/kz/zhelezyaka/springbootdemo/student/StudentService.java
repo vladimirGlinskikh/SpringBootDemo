51bb017bc6be200b9a1d7ef2c48b55d5456f7161
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-10-16 21:56:17 +0600

    Updating students

--- src/main/java/kz/zhelezyaka/springbootdemo/student/StudentService.java
+++ src/main/java/kz/zhelezyaka/springbootdemo/student/StudentService.java
@@ -1,8 +1,10 @@
 package kz.zhelezyaka.springbootdemo.student;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -33,5 +35,29 @@ public class StudentService {
                     + studentId + " does not exists");
         }
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId,
+                              String name,
+                              String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException(
+                        "student with id " + studentId + " does not exists"));
+        if (name != null &&
+                name.length() > 0 &&
+                !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+        if (email != null &&
+                email.length() > 0 &&
+                !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository
+                    .findStudentByEmail(email);
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
